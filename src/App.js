4e2c2521cecac6fabcf48657ead378ccaddf2531
Md Rasel Shah <mdraselshah56@gmail.com>
2022-03-25 21:55:07 +0600

    fake data create

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import LoadBikes from "./component/LoadBikes/LoadBikes";
 
 function App() {
+  const [bikes, setBike] = useState([]);
+  useEffect(() => {
+    fetch("FakeData.json")
+      .then((res) => res.json())
+      .then((data) => setBike(data));
+  }, []);
+
+  // console.log(bike);
+  // const { picture } = bike;
+
   return (
     <div className="App">
-      <h2>hello rect</h2>
+      <LoadBikes></LoadBikes>
     </div>
   );
 }
---
+++ src/component/LoadBikes/LoadBikes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoadBikes = () => {
+  return (
+    <div>
+      <h2>Load</h2>
+    </div>
+  );
+};
+
+export default LoadBikes;
