7aa643970fbe18693f7e981809f1c75dae401113
Md Rasel Shah <mdraselshah56@gmail.com>
2022-03-26 15:11:07 +0600

    Question add

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ function App() {
     <div className="App">
       <h1>Big Bike Hunt</h1>
       <LoadBikes></LoadBikes>
+      <h2>How React Work ?</h2>
+      <p>
+        React is a JavaScript library that creates user interfaces in a
+        predictable and efficient way using declarative code. You can use it to
+        help build single page applications.
+      </p>
+
+      <h2>Props vs State</h2>
+      <p>
+        props : <br /> 1.Read Only <br /> 2.describes what we see
+        <br />
+        State : <br /> 1. Influence Output <br /> 2. Must be declare useState()
+      </p>
     </div>
   );
 }
