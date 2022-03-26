ae8aa3727d38f8c6ce0a3bdb110cecd695dc61aa
Md Rasel Shah <mdraselshah56@gmail.com>
2022-03-26 14:53:43 +0600

    delete button working

--- src/component/BikeInfo/BIkeInfo.js
+++ src/component/BikeInfo/BIkeInfo.js
@@ -5,6 +5,7 @@ const BIkeInfo = ({ singleBike }) => {
   const { name, picture } = singleBike;
   return (
     <div className="shopping-card">
+      {/* <h1>{id}</h1> */}
       <div className="photos">
         <img src={picture} alt="" />
       </div>
--- src/component/LoadBikes/LoadBikes.js
+++ src/component/LoadBikes/LoadBikes.js
@@ -1,20 +1,35 @@
 import React, { useEffect, useState } from "react";
 import BIkeInfo from "../BikeInfo/BIkeInfo";
+import LuckyBike from "../LuckyBike/LuckyBike";
 import SetBikes from "../SetBikes/SetBikes";
 import "./LoadBikes.css";
 
 const LoadBikes = () => {
   const [bikes, setBike] = useState([]);
   const [addCart, setAddCart] = useState([]);
+  const [LuckyBiker, setLuckyBike] = useState([]);
   useEffect(() => {
     fetch("FakeData.json")
       .then((res) => res.json())
       .then((data) => setBike(data));
   }, []);
-
   const shoppingNow = (bike) => {
     const newBike = [...addCart, bike];
+    if (newBike.length > 4) {
+      alert("Your Maximum Added");
+      return;
+    }
     setAddCart(newBike);
+  };
+  const deleteSelected = () => {
+    setAddCart([]);
+  };
+
+  const luckyBike = () => {
+    const MatchBike = Math.round(Math.random() * 12);
+    setLuckyBike(addCart);
+    console.log(MatchBike);
+    return MatchBike;
   };
 
   return (
@@ -32,9 +47,10 @@ const LoadBikes = () => {
         {addCart.map((singleBike) => (
           <BIkeInfo key={singleBike.id} singleBike={singleBike}></BIkeInfo>
         ))}
+        {<LuckyBike LuckyBike={LuckyBiker}></LuckyBike>}
         <div className="lucky-bike-btn">
-          <button>Delete selected</button>
-          <button>Your lucky bike</button>
+          <button onClick={deleteSelected}>Delete selected</button>
+          <button onClick={luckyBike}>Your lucky bike</button>
         </div>
       </div>
     </div>
